260b4b941fd5a42adfcaa8ffc4d9ebdced7b3ef9
Matilda Styfberg <matildas@rackham4.uppmax.uu.se>
2023-04-12 13:37:44 +0200

    Added path to assembly, added variables to paths

--- Code/01_genome_assembly/02_quast_sbatch.sh
+++ Code/01_genome_assembly/02_quast_sbatch.sh
@@ -2,7 +2,7 @@
 #SBATCH -A uppmax2023-2-8
 #SBATCH -M snowy
 #SBATCH -p core
-#SBATCH -n 1
+#SBATCH -n 2
 #SBATCH -t 1:00:00
 #SBATCH -J quast_quality_control
 #SBATCH --mail-type=ALL
@@ -12,9 +12,14 @@
 module load bioinfo-tools
 module load quast
 
+# Variables
+INPUT="/home/matildas/GenomeAnalysis/Analysis/01_genome_assembly/01_canu/lferriphilum.contigs.fasta"
+OUTPUT="/home/matildas/GenomeAnalysis/Analysis/01_genome_assembly/02_quast_QC"
+REFERENCE="/proj/genomeanalysis2023/Genome_Analysis/2_Christel_2017/reference/OBMB01.fasta"
+
 # commands
-python quast.py REFERENCE TO ASSEMBLY \
--o /home/matildas/GenomeAnalysis/Analysis/01_genome_assembly/02_quast_QC \
--r /proj/genomeanalysis2023/Genome_Analysis/2_Christel_2017/reference/OBMB01.fasta \
--t 4
+quast.py $INPUT \
+-o $OUTPUT \
+-r $REFERENCE \
+-t 4 \
 --gene-finding
